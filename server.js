16cff57eb1e01a447216f5245effbd4637307753
Ryan Yocum <RyanCYocum@gmail.com>
2016-01-01 21:40:28 -0800

    changed port to only process.env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var Hapi = require('hapi');
 
 var server = new Hapi.Server();
-server.connection({port: process.env.PORT || 3000});
+server.connection({port: process.env.PORT});
 
 // var Sequelize = require('sequelize');
 //
